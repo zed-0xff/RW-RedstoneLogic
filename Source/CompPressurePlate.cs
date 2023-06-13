1c220935f811fe83cf097287303135db70534f1d
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2023-06-13 20:56:03 +0300

    less pressure plate defs

--- Source/CompPressurePlate.cs
+++ Source/CompPressurePlate.cs
@@ -6,10 +6,28 @@ using UnityEngine;
 
 namespace RedstoneLogic;
 
-public enum DetectType { None, Any, Pawns, Light, Heavy };
+public enum DetectType { None, Any, Pawns, Light, Heavy, Auto };
 
 public class CompPressurePlate : CompRedstonePower {
     CompProperties_PressurePlate Props => (CompProperties_PressurePlate)props;
+
+    DetectType type;
+
+    public override void PostSpawnSetup(bool respawningAfterLoad){
+        base.PostSpawnSetup(respawningAfterLoad);
+        type = Props.type;
+
+        if( type == DetectType.Auto && parent.Stuff != null ){
+            if( parent.Stuff == ThingDefOf.Gold )
+                type = DetectType.Light;
+            else if( parent.Stuff == ThingDefOf.Steel )
+                type = DetectType.Heavy;
+            else if( parent.Stuff.stuffProps.categories.Contains(StuffCategoryDefOf.Stony) )
+                type = DetectType.Pawns;
+            else
+                type = DetectType.Any;
+        }
+    }
 
     public override void CompTick(){
         if( !TransmitsPower ) return;
@@ -18,7 +36,7 @@ public class CompPressurePlate : CompRedstonePower {
             .ThingsListAtFast(parent.Position)
             .Where(t => t != parent);
 
-        switch( Props.type ){
+        switch( type ){
             case DetectType.Any:
                 if( things.Any() ){
                     powerLevel = MaxPower;
@@ -35,7 +53,7 @@ public class CompPressurePlate : CompRedstonePower {
             case DetectType.Heavy:
                 int count = things.Sum((Thing t) => t.stackCount);
                 if( count > 0 ){
-                    if( Props.type == DetectType.Heavy )
+                    if( type == DetectType.Heavy )
                         count = (int)Mathf.Ceil(count/10f);
                     powerLevel = Mathf.Min( MaxPower, count );
                     lastPoweredTick = Find.TickManager.TicksGame;
@@ -48,7 +66,7 @@ public class CompPressurePlate : CompRedstonePower {
     }
     public override string CompInspectStringExtra(){
         return base.CompInspectStringExtra() + "\n" +
-            "type: " + Props.type;
+            "type: " + type;
     }
 }
 
