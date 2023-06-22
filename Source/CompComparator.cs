90a3b73718819543fca452daa38227f565d700ca
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2023-06-22 20:40:15 +0300

    subtract mode

--- Source/CompComparator.cs
+++ Source/CompComparator.cs
@@ -2,10 +2,13 @@ using RimWorld;
 using System.Collections.Generic;
 using Verse;
 using Blocky.Core;
+using UnityEngine;
 
 namespace RedstoneLogic;
 
 public class CompComparator : CompRedstonePowerReceiver {
+    enum Mode { Compare, Subtract, Measure };
+
     struct Levels : IExposable {
         public int l,r,c,tick;
 
@@ -19,6 +22,7 @@ public class CompComparator : CompRedstonePowerReceiver {
 
     Levels[] values = new Levels[2];
     int pushTick;
+    Mode mode;
 
     public override void PostSpawnSetup(bool respawningAfterLoad){
         base.PostSpawnSetup(respawningAfterLoad);
@@ -32,17 +36,28 @@ public class CompComparator : CompRedstonePowerReceiver {
         if( pushTick != Find.TickManager.TicksGame ){
             pushTick = Find.TickManager.TicksGame;
 
+            powerLevel = 0;
             var levels = values[1 - pushTick%2];
-            if( levels.tick == pushTick-1 && levels.l <= levels.c && levels.r <= levels.c ){
-                powerLevel = levels.c;
-                if( powerLevel > 0 ){
-                    IntVec3 posOut = parent.Position + IntVec3.North.RotatedBy(parent.Rotation);
-                    var dst = CompCache<CompRedstonePower>.Get(posOut, parent.Map) as CompRedstonePowerReceiver;
-                    if( dst != null )
-                        dst.TryPushPower(powerLevel, this);
-                }
-            } else {
-                powerLevel = 0;
+
+            switch( mode ){
+                case Mode.Compare:
+                    if( levels.tick == pushTick-1 && levels.l <= levels.c && levels.r <= levels.c ){
+                        powerLevel = levels.c;
+                    }
+                    break;
+                case Mode.Subtract:
+                    if( levels.tick == pushTick-1 ){
+                        // https://minecraft.fandom.com/wiki/Redstone_Comparator#Subtract_signal_strength
+                        powerLevel = Mathf.Max(levels.c - Mathf.Max(levels.l, levels.r), 0);
+                    }
+                    break;
+            }
+
+            if( powerLevel > 0 ){
+                IntVec3 posOut = parent.Position + IntVec3.North.RotatedBy(parent.Rotation);
+                var dst = CompCache<CompRedstonePower>.Get(posOut, parent.Map) as CompRedstonePowerReceiver;
+                if( dst != null )
+                    dst.TryPushPower(powerLevel, this);
             }
         }
     }
@@ -88,9 +103,31 @@ public class CompComparator : CompRedstonePowerReceiver {
         idempotentTick();
     }
 
+    public override IEnumerable<Gizmo> CompGetGizmosExtra(){
+        foreach (Gizmo item in base.CompGetGizmosExtra()) {
+            yield return item;
+        }
+
+        yield return new Command_Action
+        {
+            icon = ContentFinder<Texture2D>.Get("RedstoneLogic/Settings"),
+                 defaultLabel = "Change mode",
+                 action = delegate {
+                     mode = mode == Mode.Compare ? Mode.Subtract : Mode.Compare;
+                 }
+        };
+    }
+
+    public override string CompInspectStringExtra(){
+        string s = base.CompInspectStringExtra() + "\n" +
+            "mode".Translate() + ": " + mode.ToString().Translate();
+        return s;
+    }
+
     public override void PostExposeData() {
         base.PostExposeData();
         Scribe_Values.Look(ref pushTick, "pushTick");
+        Scribe_Values.Look(ref mode, "mode");
         Scribe_Deep.Look(ref values[0], "value0");
         Scribe_Deep.Look(ref values[1], "value1");
     }
